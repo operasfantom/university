8d11fdd3f31b0c887b3069e655199a87c1b537b3
operasfantom <operasfantom@gmail.com>
2018-04-20 16:53:41 +0300

    HW9 hard

--- paradigms/HW9/functionalExpression.js
+++ paradigms/HW9/functionalExpression.js
@@ -2,6 +2,12 @@
 
 "use strict";
 
+var x = variable("x");
+var y = variable("y");
+var z = variable("z");
+var pi = cnst(Math.PI);
+var e = cnst(Math.E);
+
 function cnst(x) {
     return function () {
         return x
@@ -9,9 +15,13 @@ function cnst(x) {
 }
 
 function variable(name) {
-    return function (x) {
+    return function (x, y, z) {
         if (name === "x") {
             return x;
+        } else if (name === "y") {
+            return y;
+        } else if (name === "z") {
+            return z;
         } else {
             throw Error(name)
         }
@@ -31,10 +41,7 @@ function evaluate(operation) {
             arr = arr.map(function (f) {
                 return f.apply(null, parameters)
             });
-            // console.log(arr)
-            var result = arr.reduce(operation);
-            // console.log("result:", result)
-            return result;
+            return operation.apply(null, arr);
         }
     }
 }
@@ -59,18 +66,19 @@ var negate = evaluate(function (a) {
     return -a;
 });
 
+var min3 = evaluate(function (a, b, c) {
+    return Math.min(a, b, c)
+});
+
+var max5 = evaluate(function (a, b, c, d, e) {
+    return Math.max(a, b, c, d, e)
+});
+
+var min = evaluate(Math.min);
+
+var max = evaluate(Math.max);
+
 function test() {
-    /*var expr = subtract(
-        add(
-            cnst(2),
-            variable("x")
-        ),
-        cnst(3)
-    );
-
-    console.log(expr(5));
-    */
-
     var expression = add(
         subtract(
             multiply(
@@ -95,31 +103,51 @@ function test() {
 }
 
 function parse(s) {
-    var arr = s.split(' ');
+    var arr = s.split(/\s+/);
+
     //console.log(arr);
     function step(stack, token) {
 
-        function apply_binary(f) {
-            let x1 = stack.pop();
-            let x0 = stack.pop();
-            stack.push(f(x0, x1));
+        function apply(f, n) {
+            var args = stack.splice(-n, n)
+            stack.push(f.apply(null, args));
         }
 
         switch (token) {
+            case "":
+                break;
             case "+":
-                apply_binary(add);
+                apply(add, 2);
                 break;
             case "-":
-                apply_binary(subtract);
+                apply(subtract, 2);
                 break;
             case "*":
-                apply_binary(multiply);
+                apply(multiply, 2);
                 break;
             case "/":
-                apply_binary(divide);
+                apply(divide, 2);
+                break;
+            case "negate":
+                apply(negate, 1);
+                break;
+            case "pi":
+                stack.push(pi);
+                break;
+            case "e":
+                stack.push(e);
                 break;
             default:
-                //x
+                if (token.slice(0, 3) === "min") {
+                    var n = parseInt(token.slice(3, token.length));
+                    apply(min, n);
+                    break;
+                }
+                if (token.slice(0, 3) === "max") {
+                    var n = parseInt(token.slice(3, token.length));
+                    apply(max, n);
+                    break;
+                }
                 if (isNaN(token)) {
                     stack.push(variable(token));
                 } else {
@@ -134,5 +162,9 @@ function parse(s) {
 
 // test();
 
-var result = parse("x x 2 - * x * 1 +")(5);
-console.log(result);
+// var result = parse("x x 2 - * x * 1 +")(5);
+// console.log(result);
+
+// var result = parse('x y z min3')(1, 0, 0);
+// console.log(result);
+
