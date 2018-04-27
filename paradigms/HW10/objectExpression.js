26fccd94951a6309f9dde6cac6840fd0e08ed447
operasfantom <operasfantom@gmail.com>
2018-04-27 20:42:39 +0300

    HW10

--- paradigms/HW10/objectExpression.js
+++ paradigms/HW10/objectExpression.js
@@ -20,14 +20,12 @@ Expression.prototype.toString = function () {
     return result.join(" ") + " " + this.symbol;
 };
 
-function Operation(operation, symbol) {
+function Operation(operation, symbol, differentiation) {
     function Constructor() {
         this.args = arguments;
-        this.getArgs = function () {
-            return this.args;
-        };
         this.operation = operation;
         this.symbol = symbol;
+        this.diff = differentiation;
     }
 
     Object.setPrototypeOf(Constructor.prototype, Expression.prototype);
@@ -37,72 +35,287 @@ function Operation(operation, symbol) {
 
 function Const(value) {
     this.value = value;
-    this.evaluate = function () {
-        return this.value;
-    };
-    this.toString = function () {
-        return this.value.toString();
-    };
+
     Object.freeze(this);
 }
 
+Const.prototype.evaluate = function () {
+    return this.value;
+};
+
+Const.prototype.toString = function () {
+    return this.value.toString();
+};
+
+Const.prototype.diff = function () {
+    return new Const(0);
+};
+
 function Variable(name) {
     this.name = name;
-    this.evaluate = function (x, y, z) {
-        if (name === "x") {
-            return x;
-        } else if (name === "y") {
-            return y;
-        } else if (name === "z") {
-            return z;
-        } else {
-            throw Error(name + "is wrong name of Variable")
-        }
-    };
-    this.toString = function () {
-        return this.name;
-    };
     Object.freeze(this);
 }
+
+Variable.prototype.evaluate = function (x, y, z) {
+    if (this.name === "x") {
+        return x;
+    } else if (this.name === "y") {
+        return y;
+    } else if (this.name === "z") {
+        return z;
+    } else {
+        throw Error(this.name + "is wrong name of Variable")
+    }
+};
+
+Variable.prototype.toString = function () {
+    return this.name;
+};
+
+Variable.prototype.diff = function (c) {
+    if (this.name === c) {
+        return new Const(1);
+    } else {
+        return new Const(0);
+    }
+};
 
 var Add = Operation(
     function (a, b) {
         return a + b;
     },
-    "+"
+    "+",
+    function (c) {
+        return new Add(this.args[0].diff(c), this.args[1].diff(c));
+    }
 );
 
 var Subtract = Operation(
     function (a, b) {
+        // console.log(a, b);
         return a - b;
     },
-    "-"
+    "-",
+    function (c) {
+        return new Subtract(this.args[0].diff(c), this.args[1].diff(c));
+    }
 );
 
 var Multiply = Operation(
     function (a, b) {
         return a * b;
     },
-    "*"
+    "*",
+    function (c) {
+        return new Add(
+            new Multiply(this.args[0].diff(c), this.args[1]),
+            new Multiply(this.args[0], this.args[1].diff(c))
+        );
+    }
 );
 
 var Divide = Operation(
     function (a, b) {
+        // console.log(a, b);
         return a / b;
     },
-    "/"
+    "/",
+    function (c) {
+        return new Divide(
+            new Subtract(
+                new Multiply(this.args[0].diff(c), this.args[1]),
+                new Multiply(this.args[0], this.args[1].diff(c))
+            ),
+            new Multiply(this.args[1], this.args[1])
+        )
+    }
 );
 
 var Negate = Operation(
     function (a) {
         return -a;
     },
-    "negate"
+    "negate",
+    function (c) {
+        return new Negate(this.args[0].diff(c))
+    }
 );
 
-function test() {
-    var expr = new Negate(new Variable("z"));
-    assertEquals("z negate", expr.toString());
+var Square = Operation(
+    function (a) {
+        return a * a;
+    },
+    "square",
+    function (c) {
+        return new Multiply(
+            new Multiply(
+                new Const(2),
+                this.args[0]
+            ),
+            this.args[0].diff(c)
+        )
+    }
+);
+
+var Sign = Operation(
+    function (a) {
+        return a >= 0 ? +1 : -1;
+    },
+    "",
+    function (c) {
+        return 0;
+    }
+);
+
+var Abs = Operation(
+    function (a) {
+        return Math.abs(a);
+    },
+    "",
+    function (c) {
+        return new Multiply(
+            this.args[0].diff(c),
+            new Sign(this.args[0])
+        )
+    }
+);
+
+var Sqrt = Operation(
+    function (a) {
+        return Math.sqrt(Math.abs(a));
+    },
+    "sqrt",
+    function (c) {
+        return new Divide(
+            new Abs(this.args[0]),
+            new Multiply(
+                new Const(2),
+                this
+            )
+        )
+    }
+);
+
+var Power = Operation(
+    function (a, b) {
+        return Math.pow(a, b);
+    },
+    "pow",
+    function (c) {
+        return new Add(
+            new Multiply(
+                this.args[1],
+                new Multiply(
+                    new Power(
+                        this.args[0],
+                        new Subtract(
+                            this.args[1],
+                            new Const(1)
+                        )
+                    ),
+                    this.args[0].diff(c)
+                )
+            ),
+
+            new Multiply(
+                this,
+                new Multiply(
+                    this.args[1].diff(c),
+                    new Ln(this.args[0])
+                )
+            )
+        )
+    }
+);
+
+var Ln = Operation(
+    function (a) {
+        return Math.log(Math.abs(a));
+    },
+    "",
+    function (c) {
+        return new Divide(
+            new Abs(this.args[0]).diff(c),
+            new Abs(this.args[0])
+        )
+    }
+);
+
+var Log = Operation(
+    function (a, b) {
+        return Math.log(Math.abs(b)) / Math.log(Math.abs(a));
+    },
+    "log",
+    function (c) {
+        return new Divide(
+            new Ln(this.args[1]),
+            new Ln(this.args[0])
+        ).diff(c)
+    }
+);
+
+function creator(operation) {
+    function F(args) {
+        operation.apply(this, args);
+    }
+
+    F.prototype = operation.prototype;
+
+    return function () {
+        return new F(arguments);
+    }
 }
 
-// test();
+function parse(s) {
+    var arr = s.split(/\s+/);
+
+    //console.log(arr);
+    function step(stack, token) {
+
+        function apply(f, n) {
+            var args = stack.splice(-n, n);
+            stack.push(creator(f).apply(this, args));
+        }
+
+        switch (token) {
+            case "":
+                break;
+            case "+":
+                apply(Add, 2);
+                break;
+            case "-":
+                apply(Subtract, 2);
+                break;
+            case "*":
+                apply(Multiply, 2);
+                break;
+            case "/":
+                apply(Divide, 2);
+                break;
+            case "negate":
+                apply(Negate, 1);
+                break;
+            case "square":
+                apply(Square, 1);
+                break;
+            case "sqrt":
+                apply(Sqrt, 1);
+                break;
+            case "pow":
+                apply(Power, 2);
+                break;
+            case "log":
+                apply(Log, 2);
+                break;
+            default:
+                if (isNaN(token)) {
+                    stack.push(new Variable(token));
+                } else {
+                    stack.push(new Const(parseInt(token)));
+                }
+        }
+        return stack;
+    }
+
+    return arr.reduce(step, [])[0]
+}
--- paradigms/HW10/test/objectExpressionTest.js
+++ paradigms/HW10/test/objectExpressionTest.js
@@ -1,3 +1,5 @@
+// import lib from "../objectExpression.js";
+
 TestCase("UnitTest", {
     "test const": function() {
         var expr = new Const(4);
@@ -29,5 +31,40 @@ TestCase("UnitTest", {
             new Const(3)
         );
         assertEquals(7, expr.evaluate(5));
+    },
+    "test parse": function () {
+        var expr = parse('x 2 +');
+        assertEquals(7, console.log(expr.evaluate(5)));
+    },
+    "test diff": function () {
+        var expr = new Add(new Variable('x'), new Const(2)).diff('x');
+        assertEquals(1, expr.evaluate(5));
+    },
+    "test modify easy": function () {
+        var expr = new Add(new Variable('x'), new Sqrt(new Subtract(new Variable('y'), new Divide(new Square(new Const(3)), new Const(4)))));
+        assertEquals(1.5, expr.evaluate(0, 0, 0))
+    },
+    "test modify 2": function () {
+        var expr = new Sqrt(new Subtract(new Variable('y'), new Divide(new Square(new Const(3)), new Const(4)))).diff('y')
+        assertEquals(-1.0, expr.evaluate(2.00000000000000000000,2.00000000000000000000,2.00000000000000000000));
+    },
+    "test random": function () {
+        var expr = new Log(new Power(new Subtract(new Subtract(new Const(-1297918201), new Variable('y')), new Variable('x')), new Subtract(new Power(new Const(-1121176728), new Const(890377089)), new Multiply(new Variable('x'), new Const(-1429575531)))), new Negate(new Power(new Variable('x'), new Const(-1595116792))))
+    },
+    "test power diff": function () {
+        var expr = new Power(new Variable('x'), new Variable('y')).diff('x');
+        assertEquals(12, expr.evaluate(2, 3, 2));
+    },
+    "test log diff": function () {
+        var expr = new Log(new Variable('x'), new Variable('y')).diff('x');
+        assertEquals(-1 / (2 * Math.log(2)), expr.evaluate(2, 2, 2));
+    },
+    "test log diff 2": function () {
+        var expr = new Log(new Add(new Const(2), new Multiply(new Const(4), new Subtract(new Variable('x'), new Variable('z')))), new Const(1)).diff('x');
+        assertEquals(0, expr.evaluate(2, 2, 3));
+    },
+    "test simplify": function () {
+        var expr = new Variable(0).simplify();
+        console.log(expr.toString());
     }
 });
