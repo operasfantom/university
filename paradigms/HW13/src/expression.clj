db92cb59f43b099a996dc42ef1b3443a9f554f57
operasfantom <operasfantom@gmail.com>
2018-05-18 20:04:39 +0300

    HW13 corrected

---
+++ paradigms/HW13/src/expression.clj
@@ -0,0 +1,50 @@
+(def constant constantly)
+(defn variable [name] (fn [maps] (get maps name)))
+
+(defn multi-operation [f]
+  (fn [& args]
+    (fn [maps]
+      (apply f (map (fn [e] (e maps)) args))
+      )
+    ))
+
+(defn unary-operation [f]
+  (fn [arg]
+    (fn [maps]
+      (f ((fn [e] (e maps)) arg))
+      )
+    ))
+
+(def add (multi-operation +))
+(def subtract (multi-operation -))
+(def multiply (multi-operation *))
+(def divide (multi-operation (fn [^double x ^double y] (/ x y))))
+(def negate (unary-operation -))
+
+(def sinh (unary-operation (fn [x] (Math/sinh x))))
+(def cosh (unary-operation (fn [x] (Math/cosh x))))
+
+(def symbol-to-binary-operation
+  {
+   '+ add
+   '- subtract
+   '* multiply
+   '/ divide
+   'negate negate
+   'sinh sinh
+   'cosh cosh
+   })
+
+(defn parseFunction [s]
+  ((fn parse [e]
+     (cond
+       (symbol? e) (variable (str e))
+       (number? e) (constant e)
+       :else (let [op (first e) args (rest e)] (apply (get symbol-to-binary-operation op) (map parse args)))
+       )) (read-string s)))
+
+
+
+
+
+
